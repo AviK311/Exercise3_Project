c2ae537ff71748a76c3b146a5e31006ff9c9d682
Avik311 <45146057+AviK311@users.noreply.github.com>
2020-11-23 23:36:34 +0200

    added branches

--- server.js
+++ server.js
@@ -3,24 +3,22 @@
 let path = require("path");
 //express server
 const users = require('./jsons/users').users;
-
+const flowers = require('./jsons/flowers').flowers
 let express = require("express");
-const { throws } = require("assert");
 let app = express();
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname, 'views')));
 
-class User{
-    constructor(email, password){
-        this.email = email;
-        this.password = password;
-    }
-}
+
 
 app.get('/', function(req, res){
-    res.render("index", {Students: Students});
+    res.render("index");
 
 });
+
+
+
+
 
 app.listen(8071, function(){
 	console.log("running express server on 8071")
