bddd4b6dab06248536e6755e4ccfa8c2f8026176
Avik311 <45146057+AviK311@users.noreply.github.com>
2020-11-29 11:44:12 +0200

    logout logic

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let path = require("path");
 let express = require("express");
 const { BADFLAGS } = require("dns");
-let app = express();
+const app = express();
 const users = require("./jsons/users").users;
 const flowers = require("./jsons/flowers").flowers;
 const bodyParser = require('body-parser');
@@ -14,6 +14,9 @@ app.use(bodyParser.json());
 
 
 var password_attempts = 0;
+
+
+var current_user;
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
@@ -30,26 +33,32 @@ app.get("/about", function(req, res) {
 app.post("/authenticate", function(req, res) {
     let body = req.body;
     if (!validate_email(body.email)) {
-        res.json({ error: "Email is invalid" });
+        res.json({ success: false, message: "Email is invalid" });
         return;
     }
     let user = users.filter((u) => u.email == body.email);
-    console.log(user);
     if (user.length == 0)
-        res.json({ error: "There is no user with that Email Address" });
+        res.json({ success: false, message: "There is no user with that Email Address" });
     if (user.length > 1) {
         console.error("There are too many users with that Email Address");
-        res.json({ error: "Server Error!" });
+        res.json({ success: false, message: "Server Error!" });
         return;
     }
     user = user[0];
     if (user.password == body.password) {
         set_cookies(res, user);
-        res.json(user);
+        res.json({ success: true, user: user });
+        current_user = user;
     } else {
-        res.json({ error: "Wrong Password" });
+        res.json({ success: false, message: "Wrong Password" });
         password_attempts++;
     }
+});
+
+app.get("/logout", (req, res) => {
+    current_user = null;
+    set_cookies(res, { fname: '', lname: '', email: '' });
+    res.json({ success: true });
 });
 
 app.listen(8071, function() {
